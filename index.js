855beba0ad0872d9c2142d31419008ed2167a74b
Juan Rosas <rosas.juan@live.com>
2024-02-06 13:51:11 -0300

    project done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html - Allows to pick up the style.css file
+// http://expressjs.com/en/starter/static-files.html - Allows to pick up the style.css file. path arg and __dirname when using vscode
 app.use('/public', express.static(__dirname + '/public'));
 
 // http://expressjs.com/en/starter/basic-routing.html - Allows to pick up the html file
@@ -19,12 +19,36 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint... Routing
-app.get('/api/2015-12-25', function (req, res) {
-  res.json({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
+// route to /api
+app.get('/api', (req, res) => {
+  let fecha = new Date();
+  let utc = fecha.toUTCString();
+  let unix = Date.parse(utc);
+  res.json({unix, utc});
 });
 
-app.get('/api/1451001600000', (req, res) => {
-  res.json({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
+// route clicking the links
+app.get('/api/:date', (req, res) => {
+  let date = req.params.date;
+  let unix, utc;
+  if (date instanceof Date){ // if date param has date format
+    unix = date.getTime();
+    utc = date.toUTCString();
+  } else if (!isNaN(date)){ // if dara param is a number
+      unix = parseInt(date);
+      date = new Date(unix);
+      utc = date.toUTCString();
+  } else { //else try to convert it to date
+      date = new Date(date);
+      utc = date.toUTCString();
+      unix = date.getTime();
+  }
+  
+  if (utc == 'Invalid date'){ // if date param is invalid -> error
+    res.json({'error': 'Invalid date'});
+  } else { // otherwise return json
+    res.json({unix, utc});
+  }
 });
 
 // listen for requests :)
